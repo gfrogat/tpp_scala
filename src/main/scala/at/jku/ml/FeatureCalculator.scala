9e6189a727cd0795660bf8a1529966d7071d25d1
Peter Ruch <gfrogat@gmail.com>
2019-08-11 12:14:34 +0200

    Cleanup Code

--- src/main/scala/at/jku/ml/FeatureCalculator.scala
+++ src/main/scala/at/jku/ml/FeatureCalculator.scala
@@ -1,63 +1,66 @@
 package at.jku.ml
 
-import at.jku.ml.features.SparseFeature
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.functions._
-import at.jku.ml.features.{BitFeature, Feature, FrequencyFeature, SparseFeature}
-import at.jku.ml.features.BitFeature
-import java.io.{BufferedInputStream, FileInputStream, IOException}
+import java.io.IOException
 import java.nio.file.{Files, Path, Paths}
 
+import at.jku.ml.features.{BitFeature, FrequencyFeature, SparseFeature}
+import at.jku.ml.util.MolFileReader
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.functions._
 import scopt.OParser
 
-case class Config(
-    inputPath: String = null,
-    outputPath: String = null,
-    overwrite: Boolean = false,
-    featureType: String = "sparse"
-)
-
 object FeatureCalculator {
-  val calculateSparseFeatures = udf(
-    (molFile: String) => Row {
-      val molecule = MolFileReader.parseMolFile(molFile)
-      val sparseFeatures: Seq[Seq[String]] = SparseConfig.getFeatures()
-        .map { feature: SparseFeature =>
-          feature.computeFeature(molecule)
-        }
-
-      Row(sparseFeatures: _*)
-    },
-    SparseConfig.getSchema()
-  )
-
-  val calculateSemiSparseFeatures = udf(
-    (molFile: String) => Row {
-      val molecule = MolFileReader.parseMolFile(molFile)
-      val frequencyFeatures: Seq[Seq[Any]] =
-        SemiSparseConfig.getFrequencyFeatures()
-          .map { feature: FrequencyFeature =>
+  val calculateSparseFeatures: UserDefinedFunction = udf(
+    (molFile: String) =>
+      Row {
+        val molecule = MolFileReader.parseMolFile(molFile)
+        val sparseFeatures: Seq[Seq[String]] = SparseConfig
+          .getFeatures
+          .map { feature: SparseFeature =>
             feature.computeFeature(molecule)
           }
-          .flatMap { tuple =>
-            List(tuple._1, tuple._2)
+
+        Row(sparseFeatures: _*)
+      },
+    SparseConfig.getSchema
+  )
+
+  val calculateSemiSparseFeatures: UserDefinedFunction = udf(
+    (molFile: String) =>
+      Row {
+        val molecule = MolFileReader.parseMolFile(molFile)
+        val frequencyFeatures: Seq[Seq[Any]] =
+          SemiSparseConfig
+            .getFrequencyFeatures
+            .map { feature: FrequencyFeature =>
+              feature.computeFeature(molecule)
+            }
+            .flatMap { tuple =>
+              List(tuple._1, tuple._2)
+            }
+
+        val bitFeatures: Seq[Seq[Int]] =
+          SemiSparseConfig.getBitFeatures.map { feature: BitFeature =>
+            feature.computeFeature(molecule)
           }
 
-      val bitFeatures: Seq[Seq[Int]] =
-        SemiSparseConfig.getBitFeatures().map { feature: BitFeature =>
-          feature.computeFeature(molecule)
-        }
-
-      Row.merge(
-        Row(bitFeatures: _*),
-        Row(frequencyFeatures: _*)
-      )
-    },
-    SemiSparseConfig.getSchema()
+        Row.merge(
+          Row(bitFeatures: _*),
+          Row(frequencyFeatures: _*)
+        )
+      },
+    SemiSparseConfig.getSchema
   )
 
   def main(args: Array[String]): Unit = {
+    case class Config(
+        inputPath: String = null,
+        outputPath: String = null,
+        overwrite: Boolean = false,
+        featureType: String = "sparse"
+    )
+
     val builder = OParser.builder[Config]
     val parser = {
       import builder._
@@ -75,15 +78,19 @@ object FeatureCalculator {
           .action((x, c) => c.copy(outputPath = x))
           .text("output parquet file (required)"),
         opt[String]("features")
-          .valueName("(sparse|semisparse)")
+          .valueName("<feature>")
           .action((x, c) => c.copy(featureType = x))
-          .validate(x => x match {
-            case "sparse" | "semisparse" => success
-            case _ => failure("Only (sparse|semisparse) features are supported!")
-          })
-          .text("Feature type to compute"),
+          .validate(
+            x => x match {
+                case "sparse" | "semisparse" => success
+                case _ =>
+                  failure("Only [sparse, semisparse] features are supported!")
+              }
+          )
+          .text("feature type to compute [sparse, semisparse]"),
         opt[Unit]("overwrite")
           .action((_, c) => c.copy(overwrite = true))
+          .text("Overwrite output parquet file")
       )
     }
 
@@ -94,21 +101,45 @@ object FeatureCalculator {
         val writeMode: String = if (config.overwrite) "overwrite" else "error"
 
         val calculateFeatures = config.featureType match {
-          case "sparse" => calculateSparseFeatures
-          case "semisparse" => calculateSparseFeatures
+          case "sparse"     => calculateSparseFeatures
+          case "semisparse" => calculateSemiSparseFeatures
         }
 
-        if (Files.notExists(inputPath)) {
-           throw new IOException("Input filepath does not exist!")
-        }
-        if (Files.exists(outputPath)) {
-          throw new IOException("Output filepath already exists! Run with option overwrite to `--overwrite` Output file!")
-        }
-        if (inputPath == outputPath) {
-          throw new IOException("Input and Output filepath are identical!")
+        try {
+          if (Files.notExists(inputPath)) {
+            throw new IOException("Input filepath does not exist!")
+          }
+
+          if (inputPath == outputPath) {
+            throw new IOException("Input and Output filepath are identical!")
+          }
+
+          if (Files.exists(outputPath)) {
+            if (writeMode != "overwrite") {
+              throw new IOException(
+                "Output filepath already exists! Run with option overwrite to `--overwrite` Output file!"
+              )
+            }
+          }
+
+          if (!inputPath.toString.endsWith(".parquet")) {
+            throw new IllegalArgumentException(
+              "Input filepath possibly not a `.parquet` file."
+            )
+          }
+
+          if (!outputPath.toString.endsWith(".parquet")) {
+            throw new IllegalArgumentException(
+              "Output filepath possibly not a `.parquet` file."
+            )
+          }
+        } catch {
+          case iae: IllegalArgumentException =>
+            System.err.println(iae.getMessage)
+          case ioe: IOException => System.err.println(ioe.getMessage)
         }
 
-
+      /*
         val name = "SparseFeatureCalculator"
         val spark = SparkSession.builder
           .appName(name)
@@ -132,6 +163,7 @@ object FeatureCalculator {
         } finally {
           spark.stop()
         }
+       */
       case _ =>
       // arguments are bad, error message will have been displayed
     }
